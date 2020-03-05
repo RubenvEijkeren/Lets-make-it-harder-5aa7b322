1da495bb207d41620205a0beabadc621d6981605
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-03-05 11:17:44 +0100

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -1,19 +1,19 @@
 <?php
-if ($argc < 4){
+if ($argc < 4) {
 	echo "Too little arguments.".PHP_EOL;
 	die;
 }
-if ($argc > 4){
+if ($argc > 4) {
 	echo "Too many arguments".PHP_EOL;
 	die;
 }
-if ($argv[1] === $argv[2]){
+if ($argv[1] === $argv[2]) {
 	echo "Perfectly balanced, as all things should be.".PHP_EOL;
 }
 else{
 	$values = explode(',', $argv[3]);
 	foreach ($values as $value) {
-		if ($argv[1]+$value == $argv[2] || $argv[1]-$value == $argv[2]){
+		if ($argv[1]+$value == $argv[2] || $argv[1]-$value == $argv[2]) {
 			echo "Perfectly balanced, as all things should be...... but only if you add ".$value.PHP_EOL;
 			die;
 		}
