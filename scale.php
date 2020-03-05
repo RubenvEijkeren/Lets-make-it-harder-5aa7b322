875a75db8487282a2094b79ba9c3312bc2bb8fcc
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-03-05 13:07:29 +0100

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -11,13 +11,31 @@ if ($argv[1] === $argv[2]) {
 	echo "Perfectly balanced, as all things should be.".PHP_EOL;
 }
 else{
+	$diff = abs($argv[1] - $argv[2]);
+	echo $diff;
 	$values = explode(',', $argv[3]);
+	sort($values);
 	foreach ($values as $value) {
 		if ($argv[1]+$value == $argv[2] || $argv[1]-$value == $argv[2]) {
 			echo "Perfectly balanced, as all things should be...... but only if you add ".$value.PHP_EOL;
 			die;
 		}
 	}
-	echo "No balance, unacceptable.";
+	$i = 0;
+	$j = count($values)-1;
+	while ($values[$i] != $values[$j]){
+//		echo "i: " . $values[$i] ."j: " .  $values[$j]. PHP_EOL;
+		if ($values[$i] + $values[$j] == $diff) {
+			echo "Perfectly balanced, as all things should be...... but only if you add ".$values[$i] . " and " . $values[$j];
+			die;
+		}
+		if ($values[$i] + $values[$j] > $diff) {
+			$j--;
+		}
+		if ($values[$i] + $values[$j] < $diff) {
+			$i++;
+		}
+	}
+	echo "Unable to balance." . PHP_EOL;
 }
 ?>
