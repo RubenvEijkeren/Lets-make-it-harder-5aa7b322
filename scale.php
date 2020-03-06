ac7c9658bf38e0157e7f98861114ac616fa97be0
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-03-06 10:02:54 +0100

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -1,39 +1,39 @@
 <?php
 if ($argc < 4) {
-	echo "Too little arguments." . PHP_EOL;
-	die;
+    echo "Too little arguments." . PHP_EOL;
+    die;
 }
 if ($argc > 4) {
-	echo "Too many arguments" . PHP_EOL;
-	die;
+    echo "Too many arguments" . PHP_EOL;
+    die;
 }
 if ($argv[1] === $argv[2]) {
-	echo "Perfectly balanced, as all things should be.".PHP_EOL;
+    echo "Perfectly balanced, as all things should be.".PHP_EOL;
 }
 else{
-	$diff = abs($argv[1] - $argv[2]);
-	$values = explode(',', $argv[3]);
-	sort($values);
-	foreach ($values as $value) {
-		if ($argv[1] + $value == $argv[2] || $argv[1] - $value == $argv[2]) {
-			echo "Perfectly balanced, as all things should be...... but only if you add " . $value . PHP_EOL;
-			die;
-		}
-	}
-	$i = 0;
-	$j = count($values)-1;
-	while ($i != $j) {
-		if ($values[$i] + $values[$j] == $diff) {
-			echo "Perfectly balanced, as all things should be...... but only if you add " . $values[$i] . " and " . $values[$j];
-			die;
-		}
-		if ($values[$i] + $values[$j] > $diff) {
-			$j--;
-		}
-		if ($values[$i] + $values[$j] < $diff) {
-			$i++;
-		}
-	}
-	echo "Unable to balance." . PHP_EOL;
+    $diff = abs($argv[1] - $argv[2]);
+    $values = explode(',', $argv[3]);
+    sort($values);
+    foreach ($values as $value) {
+        if ($argv[1] + $value == $argv[2] || $argv[1] - $value == $argv[2]) {
+            echo "Perfectly balanced, as all things should be...... but only if you add " . $value . PHP_EOL;
+            die;
+        }
+    }
+    $i = 0;
+    $j = count($values)-1;
+    while ($i != $j) {
+        if ($values[$i] + $values[$j] == $diff) {
+            echo "Perfectly balanced, as all things should be...... but only if you add " . $values[$i] . " and " . $values[$j];
+            die;
+        }
+        if ($values[$i] + $values[$j] > $diff) {
+            $j--;
+        }
+        if ($values[$i] + $values[$j] < $diff) {
+            $i++;
+        }
+    }
+    echo "Unable to balance." . PHP_EOL;
 }
 ?>
